b1410137d53ccc4057b4975b86dc43bf394fe4e7
Jake Hall <jake.hallslm@gmail.com>
2024-08-11 08:14:37 +0100

    payment function

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { Client, TopicMessageQuery } from '@hashgraph/sdk';
+import { Client, TopicMessageQuery, TransferTransaction } from '@hashgraph/sdk';
 
 const client = Client.forTestnet().setOperator(process.env.ACCOUNT_ID, process.env.PRIVATE_KEY);
 
-const handleMessage = (message) => {
+const COMPLETE_LIST = "complete";
+
+const handleMessage = async (message) => {
   const decodedMessage = Buffer.from(message.contents, "utf8").toString()
-  console.log(`Received message: ${decodedMessage}`);
+  const { accountId, amount, cardId, newList } = JSON.parse(decodedMessage);
+  console.log(`Card: ${cardId} moved to ${newList} list.`)
+  // if moved to complete handle payment
+  if (newList.trim().toLowerCase() === COMPLETE_LIST) {
+    const tx = new TransferTransaction()
+      .addHbarTransfer(accountId, amount)
+      .addHbarTransfer(process.env.ACCOUNT_ID, -amount)
+
+    const submitResponse = await tx.execute(client);
+    const receipt = await submitResponse.getReceipt(client);
+    console.log(`Sent ${amount} HBAR to ${accountId} with transaction status: ${receipt.status.toString()}`);
+  }
 }
 
 const main = async () => {
